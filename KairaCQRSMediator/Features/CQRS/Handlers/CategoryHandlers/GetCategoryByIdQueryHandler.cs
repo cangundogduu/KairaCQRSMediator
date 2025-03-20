6ad50e8a39c2de8213a02e7b118489d6d63616be
can gündoğdu <gundogdu.can@hotmail.com>
2025-03-20 23:01:58 +0300

    Add project files.

---
+++ KairaCQRSMediator/Features/CQRS/Handlers/CategoryHandlers/GetCategoryByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using KairaCQRSMediator.DataAccess.Entities;
+using KairaCQRSMediator.Features.CQRS.Queries.CategoryQueries;
+using KairaCQRSMediator.Features.CQRS.Results.CategoryResults;
+using KairaCQRSMediator.Repositories;
+
+namespace KairaCQRSMediator.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryByIdQueryHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public GetCategoryByIdQueryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<GetCategoryByIdQueryResult> Handle(GetCategoryByIdQuery query)
+        {
+            var category = await _categoryRepository.GetByIdAsync(query.Id);
+
+            return new GetCategoryByIdQueryResult
+            {
+                CategoryId = category.CategoryId,
+                CategoryName = category.CategoryName,
+                ImageUrl = category.ImageUrl,
+                Products = category.Products
+            };
+        }
+
+
+    }
+}
---
+++ KairaCQRSMediator/Features/CQRS/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using KairaCQRSMediator.DataAccess.Entities;
+using KairaCQRSMediator.Features.CQRS.Results.CategoryResults;
+using KairaCQRSMediator.Repositories;
+
+namespace KairaCQRSMediator.Features.CQRS.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly IRepository<Category> _categoryRepository;
+
+        public GetCategoryQueryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return categories.Select(c => new GetCategoryQueryResult
+            {
+                CategoryId = c.CategoryId,
+                CategoryName = c.CategoryName,
+                ImageUrl = c.ImageUrl,
+                Products = c.Products
+            }).ToList();
+
+        }
+
+
+
+
+    }
+}
